0ff07978dc11f29f2bdd29897e5742a13019661d
WonYeChan1303 <wonyechan7772@gmail.com>
2025-01-22 21:10:39 +0900

    [1차] 비밀지도

--- test.py
+++ test.py
@@ -1,11 +1,8 @@
-def solution(arr):
+def solution(n, arr1, arr2):
     answer = []
-    for i in range(len(arr)):
-        n = i - 1
-        if n >= 0:
-            if arr[n] != arr[i]:
-                answer.append(arr[i])
-        else:
-            answer.append(arr[i]) 
-        
+
+    for i in range(n):
+        bin_map = bin(arr1[i] | arr2[i])[2:].zfill(n)
+        answer.append(bin_map.replace('1', '#').replace('0', ' '))
+    
     return answer
