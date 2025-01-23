245a23bb099e6fe440224d8f3fff14ceb2215486
WonYeChan1303 <wonyechan7772@gmail.com>
2025-01-23 18:00:25 +0900

    추억 점수

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
-def solution(n, arr1, arr2):
+def solution(name, yearning, photo):
     answer = []
+    for i in range(len(photo)):
+        count = 0
+        for j in range(len(photo[i])):
+            if photo[i][j] in name:
+                count += yearning[name.index(photo[i][j])]
+        
+        answer.append(count)
 
-    for i in range(n):
-        bin_map = bin(arr1[i] | arr2[i])[2:].zfill(n)
-        answer.append(bin_map.replace('1', '#').replace('0', ' '))
-    
     return answer
