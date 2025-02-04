77e2741677fcad188a109ef95b666c32d2694437
WonYeChan1303 <wonyechan7772@gmail.com>
2025-02-04 22:23:16 +0900

    폰켓몬

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-def solution(name, yearning, photo):
-    answer = []
-    for i in range(len(photo)):
-        count = 0
-        for j in range(len(photo[i])):
-            if photo[i][j] in name:
-                count += yearning[name.index(photo[i][j])]
-        
-        answer.append(count)
+def solution(nums):
+    answer = 0
+    pokemon = len(set(nums))
+
+    answer = min(pokemon, len(nums)//2)
 
     return answer
