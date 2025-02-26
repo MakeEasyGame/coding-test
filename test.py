7f6decf81f7d508f80bc923bbe3def6483d26e60
WonYeChan1303 <wonyechan7772@gmail.com>
2025-02-26 21:12:24 +0900

    데이터 분석

--- test.py
+++ test.py
@@ -1,23 +1,11 @@
-# 크레인 인형뽑기 게임
+# 데이터 분석
 
-def solution(board, moves):
-    answer = 0
-    basket = []
-    
-    for i in moves:
-        col = [row[i-1] for row in board]
-        l = next((item for item in col if item != 0), 0)
+def solution(data, ext, val_ext, sort_by):
+    answer = []
+    dict = {'code': 0, 'date': 1, 'maximum': 2, 'remain': 3}
 
-        if l != 0:
-            for row in board:
-                if row[i-1] == l:
-                    row[i-1] = 0
-                    break
-        print(l)
-        if len(basket) > 0 and l == basket[len(basket)-1] and l != 0:
-            answer+=2
-            basket.pop()
-        else:
-            basket.append(l)
-    
-    return answer
+    for d in data:
+        if d[dict[ext]] < val_ext:
+            answer.append(d)
+
+    return sorted(answer, key = lambda x:x[dict[sort_by]])
