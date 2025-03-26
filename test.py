c4b44b2f6be5b5724f9f6de47bfc9252f15278a4
WonYeChan1303 <wonyechan7772@gmail.com>
2025-03-26 22:58:41 +0900

    [3차] N진수 게임

--- test.py
+++ test.py
@@ -1,13 +1,27 @@
-# 전화번호 목록
+# [3차] N진수 게임
 
-def solution(phone_book):
-    answer = True
+def solution(n, t, m, p):
+    answer, nums = '', ''
+    cnt, idx = 0, 0
+    while cnt < p + m * t:
+        num = str(to_base(cnt, n))
+        nums += num[idx]
+        idx+=1
+        if idx >= len(num):
+            cnt+=1
+            idx=0
 
-    phone_book.sort()
-
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            answer = False
-            break
-
+    answer = ''.join(map(lambda x: nums[x-1], (map(lambda x: p + m * x, range(t)))))
     return answer
+
+def to_base(num, base):
+    digits = "0123456789ABCDEF"
+    result = ""
+    if num == 0:
+        return "0"
+    while num > 0:
+        result = digits[num % base] + result
+        num //= base
+    return result
+
+print(solution(16,16,2,1))
