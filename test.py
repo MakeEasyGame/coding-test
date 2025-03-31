3da865293905d5fadef98d9024bf278f84f5bd55
WonYeChan1303 <wonyechan7772@gmail.com>
2025-03-31 22:12:31 +0900

    압축

--- test.py
+++ test.py
@@ -1,27 +1,16 @@
-# [3차] N진수 게임
+# 압축
 
-def solution(n, t, m, p):
-    answer, nums = '', ''
-    cnt, idx = 0, 0
-    while cnt < p + m * t:
-        num = str(to_base(cnt, n))
-        nums += num[idx]
-        idx+=1
-        if idx >= len(num):
-            cnt+=1
-            idx=0
-
-    answer = ''.join(map(lambda x: nums[x-1], (map(lambda x: p + m * x, range(t)))))
+def solution(msg):
+    w = ''
+    dict = {i: ord(i)-64 for i in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'}
+    answer = []
+    for c in msg:
+        print(w, c)
+        if w + c in dict.keys():
+            w = w + c
+        else:
+            dict[w + c] = max(dict.values()) + 1
+            answer.append(dict[w])
+            w = c
+    answer.append(dict[w])
     return answer
-
-def to_base(num, base):
-    digits = "0123456789ABCDEF"
-    result = ""
-    if num == 0:
-        return "0"
-    while num > 0:
-        result = digits[num % base] + result
-        num //= base
-    return result
-
-print(solution(16,16,2,1))
