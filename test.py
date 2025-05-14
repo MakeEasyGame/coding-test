7d9fbb030a841ce0cbc46aebe4dfc71e4db8e7d8
WonYeChan1303 <wonyechan7772@gmail.com>
2025-05-14 23:00:52 +0900

    오픈채팅방

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-# 2 x n 타일링
+# 오픈채팅방
 
-def solution(n):
-    if n <= 2:
-        return n
-    a, b = 1, 2
-    for _ in range(3, n + 1):
-        a, b = b, (a + b) % 1000000007
-    return b
+def solution(record):
+    answer = []
+    lst = []
+    dict = {}
+    for r in record:
+        info = r.split(' ')
+        if info[0] == 'Enter':
+            dict[info[1]] = info[2]
+            lst.append(f'{info[1]} 님이 들어왔습니다.')
+        elif info[0] == 'Leave':
+            lst.append(f'{info[1]} 님이 나갔습니다.')
+        else:
+            dict[info[1]] = info[2]
+    for i in lst:
+        s = i.split(' ')
+        answer.append(dict[s[0]]+s[1]+' '+s[2])
+    return answer
