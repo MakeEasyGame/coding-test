bbcf562f11a4fc46dbe82e451450ab0d457e578f
WonYeChan1303 <wonyechan7772@gmail.com>
2025-05-16 22:40:32 +0900

    숫자 변환하기

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
-# 오픈채팅방
+# 숫자 변환하기
 
-def solution(record):
-    answer = []
-    lst = []
-    dict = {}
-    for r in record:
-        info = r.split(' ')
-        if info[0] == 'Enter':
-            dict[info[1]] = info[2]
-            lst.append(f'{info[1]} 님이 들어왔습니다.')
-        elif info[0] == 'Leave':
-            lst.append(f'{info[1]} 님이 나갔습니다.')
-        else:
-            dict[info[1]] = info[2]
-    for i in lst:
-        s = i.split(' ')
-        answer.append(dict[s[0]]+s[1]+' '+s[2])
-    return answer
+def solution(x, y, n):
+    queue = [(x, 0)]
+    visited = set()
+    visited.add(x)
+    idx = 0
+    while idx < len(queue):
+        current, cnt = queue[idx]
+        idx += 1
+        if current == y:
+            return cnt
+        for i in (current+n, current*2, current*3):
+            if i <= y and i not in visited:
+                visited.add(i)
+                queue.append((i, cnt + 1))
+
+    return -1
