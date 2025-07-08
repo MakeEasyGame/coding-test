57d46314614ea800d5bddabdccc2ec6add0be6f0
WonYeChan1303 <wonyechan7772@gmail.com>
2025-07-09 00:00:54 +0900

    약수들의 합

--- baekjoon.py
+++ baekjoon.py
@@ -1,10 +1,16 @@
-a,b = map(int, input().split())
-cnt = 0
-for i in range(1, a+1):
-    if a % i == 0:
-        cnt+=1
-        if cnt == b:
-            print(i)
-            break
-if cnt < b:
-    print(0)
+import math
+while(True):
+    a = int(input())
+    if a == -1:
+        break
+    lst = []
+    for i in range(1, int(math.sqrt(a))+1):
+        if a % i == 0:
+            lst.append(i)
+            if a//i != a:
+                lst.append(a//i)
+    lst.sort()
+    if a == sum(lst):
+        print(a, '=', ' + '.join(map(str, lst)))
+    else:
+        print(a, 'is NOT perfect.')
