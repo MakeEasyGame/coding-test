eb55b04acacb8505f79a20a2f4513580ef88ef73
WonYeChan1303 <wonyechan7772@gmail.com>
2025-07-09 00:00:28 +0900

    약수 구하기

--- baekjoon.py
+++ baekjoon.py
@@ -1,13 +1,10 @@
-try:
-    while True:
-        a,b = map(int, input().split())
-        if a==0 and b==0:
+a,b = map(int, input().split())
+cnt = 0
+for i in range(1, a+1):
+    if a % i == 0:
+        cnt+=1
+        if cnt == b:
+            print(i)
             break
-        elif b % a == 0:
-            print("factor")
-        elif a % b == 0:
-            print("multiple")
-        else:
-            print("neither")
-except EOFError:
-    pass
+if cnt < b:
+    print(0)
