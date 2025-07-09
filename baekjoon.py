73d68cb8d6238262634dc3912a60b3edfb33244b
WonYeChan1303 <wonyechan7772@gmail.com>
2025-07-09 23:16:23 +0900

    최소공배수

--- baekjoon.py
+++ baekjoon.py
@@ -1,24 +1,6 @@
-import sys
 import math
-a = int(sys.stdin.readline())
+a = int(input())
 
-if a == 1:
-    print("")
-else:
-    def isPrime(n):
-        for i in range(2, int(math.sqrt(n))+1):
-            if n % i == 0:
-                return False
-        return True
-
-    result = []
-    n = a
-    while not isPrime(n):
-        for i in range(2, int(math.sqrt(a))+1):
-            if n % i == 0 and isPrime(i):
-                n = n//i
-                result.append(i)
-    if n != 1:
-        result.append(n)
-    result.sort()
-    print(a if not result else '\n'.join(map(str, result)))
+for i in range(a):
+    a, b = map(int, input().split())
+    print(math.lcm(a, b))
